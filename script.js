61b1b8068b890c9c0e9673f29ad4d9a12be91d13
djenidisimple <djenidigauss@gmail.com>
2025-12-01 01:11:27 +0300

    i prepare design for dektop

---
+++ script.js
@@ -0,0 +1,19 @@
+let btnShare = document.querySelector(".btn-share");
+let btnShare2 = document.querySelector(".btn-share-2");
+let share = document.querySelector('.share')
+
+btnShare.addEventListener("click", function () {
+    share.style.display = 'flex';
+    btnShare.style.backgroundColor = 'var(--Desaturated-dark-blue)';
+    btnShare.style.backgroundImage = 'var(--icon-share-2)';
+});
+
+// btnShare.addEventListener("dblclick", function () {
+//     share.style.display = "none";
+//     btnShare.style.backgroundColor = 'var(--light-grayish-blue)';
+//     btnShare.style.backgroundImage = 'var(--icon-share)';
+// })
+
+btnShare2.addEventListener("click", function () {
+    share.style.display = 'none'
+});
